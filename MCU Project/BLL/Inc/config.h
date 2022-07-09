cf34ecc06eab2800799dfe8e921bc082857c6b7d
m-RNA <m-RNA@qq.com>
2022-07-09 20:35:11 +0800

    补充配置文件 预计支持msp432,stm32f103,stm32g431

--- MCU Project/BLL/Inc/config.h
+++ MCU Project/BLL/Inc/config.h
@@ -1,12 +1,28 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#define ADC_SAMPLING_NUM 1024
+#if defined STM32F103xE || STM32F103xB
+#define TimerSourerFreq 72000000
+#define SignalSampleFreq_MAX 1000000
+
+#elif defined STM32G431xx
+#define TimerSourerFreq 170000000
+#define SignalSampleFreq_MAX 2000000
+
+#elif defined __MSP432P401R__
 #define TimerSourerFreq 48000000
-#define SignalSample_Freq_Multiple 16 // 采样频率设定为信号基波频率的几倍（Fs = ？F0）
-#define SignalSample_Freq_MAX 1000000
-#define SignalSample_Period_MIN (TimerSourerFreq / SignalSample_Freq_MAX)
+#define SignalSampleFreq_MAX 1000000
+
+#endif
+
+#define ADC_SAMPLING_NUM 1024
+#define SignalSampleFreq_Multiple 16 // 采样频率设定为信号基波频率的几倍（Fs = ？F0）
+#define SignalSamplePeriod_MIN (TimerSourerFreq / SignalSampleFreq_MAX)
 
 #define DEBUG
+
+#if (ADC_SAMPLING_NUM != 1024)
+#warning ADC_SAMPLING_NUM should be 1024
+#endif
 
 #endif
