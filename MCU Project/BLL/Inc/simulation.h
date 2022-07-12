7faa6482b1410a5a8a6fb5b3a01df3089cf2db23
m-RNA <m-RNA@qq.com>
2022-07-12 14:40:44 +0800

    添加了一点注释

--- MCU Project/BLL/Inc/simulation.h
+++ MCU Project/BLL/Inc/simulation.h
@@ -2,12 +2,13 @@
 #define __STIMULATION_H
 #include "config.h"
 
-#define Simulate_Sample_ADC_Noise 100
-#define Simulate_WaveformDate_Period_Length SignalSampleFreq_Multiple
+#define Simulation_Times 7            // 仿真次数
+#define Simulate_Sample_ADC_Noise 100 // ADC采样噪声
 
-extern u8 Simulation_Times_Index;
+extern u8 Simulation_Times_Index; // 仿真次数索引
+extern u32 Simulation_CCR[10];    // 仿真捕获值
 
-void Simulate_Signal_WaveformData(u16 *SimulateWaveData);
-void Simulate_Signal_Synthesizer(u16 *SimulateWaveData);
+void Simulate_Signal_Synthesizer(u16 *SimulateWaveData);  // 信号合成
+void Simulate_Signal_WaveformData(u16 *SimulateWaveData); // 常见信号
 
 #endif
--- MCU Project/BLL/simulation.c
+++ MCU Project/BLL/simulation.c
@@ -2,20 +2,24 @@
 #include "arm_math.h"
 #include "log.h"
 #include "stdlib.h"
-
+#ifdef Simulation
+extern u8 Simulation_Times_Index;
+extern u32 Simulation_CCR[10];
+#endif
+#define Simulate_WaveformDate_Period_Length SignalSampleFreq_Multiple
 #define Add_Noise (rand() % Simulate_Sample_ADC_Noise)
-
-u8 Simulation_Times_Index  = 0;
+u8 Simulation_Times_Index = 0;
 
 u32 Simulation_CCR[10] = {
-    TimerSourerFreq /   1000, 
-    TimerSourerFreq /  50000, 
-    TimerSourerFreq / 100000, 
-    
-    TimerSourerFreq /  30000, 
-    TimerSourerFreq / 600000, 
-    TimerSourerFreq / 900000, 
-    SignalSamplePeriod_MIN
+    TimerSourerFreq / 1000,   // 电赛测试信号1
+    TimerSourerFreq / 50000,  // 电赛测试信号2
+    TimerSourerFreq / 100000, // 电赛测试信号3
+
+    SignalSamplePeriod_MIN, // 最大采样率
+
+    TimerSourerFreq / 30000, // 自定义
+    TimerSourerFreq / 600000,
+    TimerSourerFreq / 900000,
 };
 
 #define Precision 10 // 精度 - 到几次谐波
@@ -24,10 +28,11 @@ float Simulation_NormAm[10][Precision - 1] = {
     {0.00f, 0.08f, 0.15f, 0.00f}, // 电赛测试信号2
     {0.00f, 0.00f, 0.00f, 0.10f}, // 电赛测试信号3
 
-    {0.00f, -0.1111111111f, 0.00f, 0.04f, 0.0f, -0.0204081633f, 0.0f, 0.0123456790f}, // 三角波
-    {0.00f, 0.3333333333f, 0.0f, 0.2f, 0.0f, 0.1428571429f, 0.0f, 0.1111111111f}, // 方波
-    {0.5f, 0.3333333333f, 0.25f, 0.2f, 0.1666666667f, 0.1428571429f, 0.125f, 0.1111111111f}, // 锯齿波？
     {0.0f, 0.0f, 0.0f, 0.0f}, // 正弦波
+
+    {0.00f, -0.1111111111f, 0.00f, 0.04f, 0.0f, -0.0204081633f, 0.0f, 0.0123456790f},        // 三角波
+    {0.00f, 0.3333333333f, 0.0f, 0.2f, 0.0f, 0.1428571429f, 0.0f, 0.1111111111f},            // 方波
+    {0.5f, 0.3333333333f, 0.25f, 0.2f, 0.1666666667f, 0.1428571429f, 0.125f, 0.1111111111f}, // 锯齿波
 };
 
 extern u16 Compare_Min(float Mag[], u16 len);
@@ -54,18 +59,18 @@ void Simulate_Signal_Synthesizer(u16 *SimulateWaveData)
         SimulateWaveData[i] = 1000 * (OWaveDate[i] - OWaveDate[MinIndex]);
         // 这个1000是随便定的，不要太大就好了，目的是把小数转换为整数
     }
-    
+
     // 复制数据
-    for(u16 i = 1; i < ADC_SAMPLING_NUM / Simulate_WaveformDate_Period_Length; ++i)
+    for (u16 i = 1; i < ADC_SAMPLING_NUM / Simulate_WaveformDate_Period_Length; ++i)
     {
-        for(u16 j = 0; j < Simulate_WaveformDate_Period_Length; ++j)
+        for (u16 j = 0; j < Simulate_WaveformDate_Period_Length; ++j)
         {
             SimulateWaveData[j + i * Simulate_WaveformDate_Period_Length] = SimulateWaveData[j] + Add_Noise;
         }
     }
-    for(u16 j = 0; j < Simulate_WaveformDate_Period_Length; ++j)
+    for (u16 j = 0; j < Simulate_WaveformDate_Period_Length; ++j)
     {
-        SimulateWaveData[j]+=Add_Noise;
+        SimulateWaveData[j] += Add_Noise;
     }
 }
 
@@ -75,12 +80,12 @@ static u16 Simulation_ADC_Data[Simulate_WaveformDate_Period_Length] = {0};
 static void SquareWaveOut(void)
 {
     u16 i;
-	for(i = 0; i < Simulate_WaveformDate_Period_Length; i++)
+    for (i = 0; i < Simulate_WaveformDate_Period_Length; i++)
     {
-        if (i < Simulate_WaveformDate_Period_Length>>1)
+        if (i < Simulate_WaveformDate_Period_Length >> 1)
             Simulation_ADC_Data[i] = 4095;
         else
-            Simulation_ADC_Data[i] = 0;  
+            Simulation_ADC_Data[i] = 0;
     }
 }
 
@@ -88,14 +93,14 @@ static void SquareWaveOut(void)
 static void TriangularWaveOut(void)
 {
     u16 i, j;
-	for(i = 0, j = 0; i < Simulate_WaveformDate_Period_Length; i++)
+    for (i = 0, j = 0; i < Simulate_WaveformDate_Period_Length; i++)
     {
-        Simulation_ADC_Data[i] =  j * 2 * 4095 / Simulate_WaveformDate_Period_Length;
-        
-        if (i < Simulate_WaveformDate_Period_Length>>1)
+        Simulation_ADC_Data[i] = j * 2 * 4095 / Simulate_WaveformDate_Period_Length;
+
+        if (i < Simulate_WaveformDate_Period_Length >> 1)
             j++;
         else
-            j--;       
+            j--;
     }
 }
 
@@ -103,49 +108,46 @@ static void TriangularWaveOut(void)
 static void SawtoothWaveOut(void)
 {
     u16 i;
-	for(i = 0; i < Simulate_WaveformDate_Period_Length; i++)
+    for (i = 0; i < Simulate_WaveformDate_Period_Length; i++)
     {
-        Simulation_ADC_Data[i] =  i * 4096 / Simulate_WaveformDate_Period_Length;
+        Simulation_ADC_Data[i] = i * 4096 / Simulate_WaveformDate_Period_Length;
     }
 }
 
 static void SinWaveOut(void)
 {
-	u16 i;
-	for (i = 0; i < Simulate_WaveformDate_Period_Length; i++)
-	{
-		Simulation_ADC_Data[i] = (u16)(2090 + 1990 * arm_sin_f32((2 * PI * i) / Simulate_WaveformDate_Period_Length));
-	}
+    u16 i;
+    for (i = 0; i < Simulate_WaveformDate_Period_Length; i++)
+    {
+        Simulation_ADC_Data[i] = (u16)(2090 + 1990 * arm_sin_f32((2 * PI * i) / Simulate_WaveformDate_Period_Length));
+    }
 }
 
 void Simulate_Signal_WaveformData(u16 *SimulateWaveData)
 {
-    switch(Simulation_Times_Index)
+    switch (Simulation_Times_Index)
     {
-        case 0:
-            SquareWaveOut();
-            break;
-        case 1:
-            TriangularWaveOut();
-            break;
-        case 2:
-            SawtoothWaveOut();
-            break;    
-        case 3:
-            SinWaveOut();
-            break;
-        default:
-            log_debug("It is same Simulate_WaveformDate!!!");
-            break;
+    case 0:
+        SquareWaveOut();
+        break;
+    case 1:
+        TriangularWaveOut();
+        break;
+    case 2:
+        SawtoothWaveOut();
+        break;
+    case 3:
+        SinWaveOut();
+        break;
+    default:
+        log_debug("It is same Simulate_WaveformDate!!!");
+        break;
     }
-    for(u16 i = 0; i < ADC_SAMPLING_NUM / Simulate_WaveformDate_Period_Length; ++i)     // 复制数据
+    for (u16 i = 0; i < ADC_SAMPLING_NUM / Simulate_WaveformDate_Period_Length; ++i) // 复制数据
     {
-        for(u16 j = 0; j < Simulate_WaveformDate_Period_Length; ++j)
+        for (u16 j = 0; j < Simulate_WaveformDate_Period_Length; ++j)
         {
             SimulateWaveData[j + i * Simulate_WaveformDate_Period_Length] = Simulation_ADC_Data[j] + Add_Noise;
         }
-    } 
+    }
 }
-
-
-
