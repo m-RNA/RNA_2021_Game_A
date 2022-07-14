204b4d9186f046a6289122e4c125645ecbf81d90
m-RNA <m-RNA@qq.com>
2022-07-14 14:18:29 +0800

    小修改stm32工程

--- MCU Project/MCU/Stm32F103/Core/Src/adc.c
+++ MCU Project/MCU/Stm32F103/Core/Src/adc.c
@@ -57,7 +57,7 @@ void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
